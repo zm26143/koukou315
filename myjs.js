98d38912f716ccccda4ed5923d825943ed49f666
zm26143 <zm26143@josai.ac.jp>
2026-06-29 12:11:02 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,6 +1,10 @@
 alert('Hello, kousirou!');
 alert(1+1);
 alert(Math.sqrt(1+1));
+• 2の平方根と3の平方根の和を計算しなさい。
+• 1を0で割るとどうなるか、試してみなさい。
+• 0を0で割るとどうなるか、試してみなさい。
+Infinity は、計算結果がJavaScriptが扱える限界
 alert(2 + 3);
 alert('2' + '3');
 alert(Number('2') + Number('3'));
